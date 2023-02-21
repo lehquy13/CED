576d63826399e9af44b8d813e58fa4c30f76bdf7
lehquy13 <lehquy13@gmail.com>
2023-02-21 15:46:33 +0700

    add 2 simple intergration test for services

---
+++ test/CED.Application.Tests/ClassInformations/ClassInformationAppService_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace CED.ClassInformations
+{
+    public class ClassInformationAppService_Tests: CEDApplicationTestBase
+    {
+        private readonly IClassInformationAppService _classInformationAppService;
+
+        public ClassInformationAppService_Tests()
+        {
+            _classInformationAppService = GetRequiredService<IClassInformationAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Class()
+        {
+            //Act
+            var result = await _classInformationAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            //result.Items.ShouldContain(b => b. == "1984");
+        }
+    }
+}
---
+++ test/CED.Application.Tests/ClassInformations/SubjectAppService_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace CED.ClassInformations
+{
+    public class SubjectAppService_Tests : CEDApplicationTestBase
+    {
+        private readonly ISubjectAppService _subjectAppService;
+
+        public SubjectAppService_Tests()
+        {
+            _subjectAppService = GetRequiredService<ISubjectAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Class()
+        {
+            //Act
+            var result = await _subjectAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            //result.Items.ShouldContain(b => b. == "1984");
+        }
+    }
+}
